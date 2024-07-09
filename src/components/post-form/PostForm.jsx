08afb7af7fb7f9970cc66889c88ea2115fd1f871
SprihaAnand <21bcs040@nith.ac.in>
2024-07-09 18:22:56 +0530

    post-form

---
+++ src/components/post-form/PostForm.jsx
@@ -0,0 +1,17 @@
+import {useCallback} from 'react'
+import { useForm } from 'react-hook-form'
+import {Button, Input, Select, RTE} from '../index'
+import appwriteService from "../../appwrite/config"
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+function PostForm() {
+    
+    const {register, handleSubmit, watch, setValue, control, getValues} = useForm()
+
+  return (
+    <div>PostForm</div>
+  )
+}
+
+export default PostForm
