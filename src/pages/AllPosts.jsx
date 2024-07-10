ee371b0d8253d8385d313bb791618c6fae4e32c6
SprihaAnand <21bcs040@nith.ac.in>
2024-07-10 21:10:27 +0530

    pages added

---
+++ src/pages/AllPosts.jsx
@@ -0,0 +1,30 @@
+import {useState, useEffect} from 'react'
+import { PostCard, Container } from '../components'
+import appwriteService from "../appwrite/config"
+
+function AllPosts() {
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        appwriteService.getPosts([]).then((posts) => {
+        if(posts) {
+            setPosts(posts.documents)
+        }
+    })} , [])
+    
+
+  return (
+    <div className='w-full py-8'>
+        <Container>
+            <div className='flex flex-wrap'>
+                {posts.map((post) => (
+                    <PostCard key={post.$id} post={post}/>
+                ))}
+            </div>
+        </Container>
+    </div>
+  )
+}
+
+export default AllPosts
